3d34b9c33b4841a40a5d67b0ce6c9554265cddbd
Bhumi <bhumi.bhusal@northwestern.edu>
2022-03-27 14:16:38 -0500

    what is being added/changed

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -32,10 +32,6 @@ with open(csvpath) as csvfile:
 #to calculate total number of votes
 Total_votes = len(Ballot_ID)
 
-print("Election Results")
-print("--------------------------------------")
-print(f"Total Votes:  {Total_votes}")
-print("--------------------------------------")
 
 #defining election results as dictionary with candidates as key
 Election_results = {}
@@ -53,22 +49,41 @@ for vote in Candidate:
 
 
 # printing total votes count for each candidate
-   
-for key in Election_results.keys():
-    Percentage_vote = 100*Election_results[key]/Total_votes
-    Percentage_vote = round(Percentage_vote,2)
-    print(f"{key}:{Election_results[key]} ({Percentage_vote}%)")
 
-#Defining a funciton to find the winner from the dictionary with maximum votes
-def keywithmaxval(d):
-     """ a) create a list of the dict's keys and values; 
-         b) return the key with the max value"""  
-     v=list(d.values())
-     k=list(d.keys())
-     return k[v.index(max(v))]
+#giving path for text print to file
+path = os.path.join("PyPoll.txt")
+with open(path, "w+") as file:
+
+    message1 = "Election Results"
+    file.write(f"{message1}\n")
+    print("message1")
+    print("--------------------------------------")
+    message2 = f"Total Votes:  {Total_votes}"
+    print(message2)
+    file.write(f"{message2}\n")
+    print("--------------------------------------")
+    
+    #calculating percentage votes and printing results
+    for key in Election_results.keys():
+        Percentage_vote = 100*Election_results[key]/Total_votes
+        Percentage_vote = round(Percentage_vote,2)
+        message3 = f"{key}:{Election_results[key]} ({Percentage_vote}%)"
+        print(message3)
+        file.write(f"{message3}\n")
 
 
-#printing winner
-print("------------------------------------------")
-print(f"Winner: {keywithmaxval(Election_results)}")
-print("-------------------------------------------")
+    #Defining a funciton to find the winner from the dictionary with maximum votes
+    def keywithmaxval(d):
+        """ a) create a list of the dict's keys and values; 
+            b) return the key with the max value"""  
+        v=list(d.values())
+        k=list(d.keys())
+        return k[v.index(max(v))]
+
+
+    #printing winner
+    print("------------------------------------------")
+    message4 = f"Winner: {keywithmaxval(Election_results)}"
+    print(message4)
+    file.write(f"{message4}\n")
+    print("-------------------------------------------")
